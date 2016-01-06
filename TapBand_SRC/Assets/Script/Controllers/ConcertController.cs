50533cfad676ead580b9a38a1bca0a7fd9a478fa
sztyop <sztyop01@gmail.com>
2016-01-07 00:11:17 +0100

    Concert Controller second commit

--- TapBand_SRC/Assets/Script/Controllers/ConcertController.cs
+++ TapBand_SRC/Assets/Script/Controllers/ConcertController.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class ConcertController : MonoBehaviour {
 
 	private HudUI hud;
-	
+	private SongController  songController;
+
+	public delegate void RestartConcertEvent();
+	public event RestartConcertEvent RestartConcert;
+
+
+
 	
 	void Awake()
 	{
@@ -15,18 +21,41 @@ public class ConcertController : MonoBehaviour {
 	void OnEnable()
 	{
 		hud.NewConcert += DisplayNewConcert;
+		songController.GiveNextSong += GiveNextSongFromConcert;
+
 	}
 	void OnDisable()
 	{
 		hud.NewConcert -= DisplayNewConcert;
+		songController.GiveNextSong -= GiveNextSongFromConcert;
 	}
 	
+	//mi a kövtetkező lejátszandó szám, értesíti
+	private SongData GiveNextSongFromConcert()
+	{
+		ConcertState state = GameState.instance.Concert;
 
-	
+		//ha boss battle
+		if (state.CurrentSong.bossBattle) 
+		{
+			//új indítása
+			state.CurrentConcertID = state.GetNextConcert().id;
+		}
+
+
+		state.LastComplatedSongID = state.CurrentSong.id;
+		state.CurrentSong = state.GetNextSong();
+
+		return state.CurrentSong;
+	}
+
 	private string DisplayNewConcert()
 	{
 		return GameState.instance.Concert.CurrentConcert.name;
 	}
-	
+
+
+
+
 
 }
--- TapBand_SRC/Assets/Script/Model/State/ConcertState.cs
+++ TapBand_SRC/Assets/Script/Model/State/ConcertState.cs
@@ -7,7 +7,8 @@ public class ConcertState {
 	private int currentConcertID;
 	private int lastComplatedSongID;
 	private SongData currentSong;
-	
+
+
 	
 	public int CurrentConcertID
 	{
@@ -34,7 +35,28 @@ public class ConcertState {
 	}
 
 	
-	
+	//geci gány, proto jó lesz
+	public SongData GetNextSong()
+	{
+		return GameData.instance.SongDataList.Find ( x => x.id == currentSong.id+1);
+	}
+
+	public ConcertData GetNextConcert()
+	{
+		return GameData.instance.ConcertDataList.Find ( x => x.id == CurrentConcert.id+1);
+	}
+
+	public SongData CurrentSong
+	{
+		get
+		{
+			return currentSong;
+		}
+		set
+		{
+			currentSong = value;
+		}
+	}
 	
 	public ConcertData CurrentConcert
 	{
