0fc8c51df36c4e32358d041554f405cbc0a71111
pappszi <papp.szilard.0621@gmail.com>
2016-01-07 17:40:33 +0100

    EquipmentState, ListUtils
    EquipmentState, ListUtils

--- TapBand_SRC/Assets/Script/Model/State/EquipmentState.cs
+++ TapBand_SRC/Assets/Script/Model/State/EquipmentState.cs
@@ -3,6 +3,129 @@ using System.Collections;
 
 [System.Serializable]
 public class EquipmentState {
+	private int drumEquipmentId;
+	private int guitarEquipmentId;
+	private int bassEquipmentId;
+	private int keyboardEquipmentId;
 
+	public int DrumEquipmentId
+	{
+		get
+		{
+			return drumEquipmentId;
+		}
+		
+		set
+		{
+			drumEquipmentId = value;
+		}
+	}
+
+	public int GuitarEquipmentId
+	{
+		get
+		{
+			return guitarEquipmentId;
+		}
+		
+		set
+		{
+			guitarEquipmentId = value;
+		}
+	}
+
+	public int BassEquipmentId
+	{
+		get
+		{
+			return bassEquipmentId;
+		}
+		
+		set
+		{
+			bassEquipmentId = value;
+		}
+	}
+
+	public int KeyboardEquipmentId
+	{
+		get
+		{
+			return keyboardEquipmentId;
+		}
+		
+		set
+		{
+			keyboardEquipmentId = value;
+		}
+	}
+
+	public EquipmentData CurrentDrumEquipment
+	{
+		get
+		{
+			if (drumEquipmentId == 0)
+			{
+				return null;
+			}
+			return GameData.instance.EquipmentDataList.Find(x => x.id == drumEquipmentId);
+		}
+	}
+
+	public EquipmentData CurrentGuitarEquipmentId
+	{
+		get
+		{
+			if (guitarEquipmentId == 0)
+			{
+				return null;
+			}
+			return GameData.instance.EquipmentDataList.Find(x => x.id == guitarEquipmentId);
+		}
+	}
+
+	public EquipmentData CurrentBassEquipmentId
+	{
+		get
+		{
+			if (bassEquipmentId == 0)
+			{
+				return null;
+			}
+			return GameData.instance.EquipmentDataList.Find(x => x.id == bassEquipmentId);
+		}
+	}
+
+	public EquipmentData CurrentKeyboardEquipmentId
+	{
+		get
+		{
+			if (keyboardEquipmentId == 0)
+			{
+				return null;
+			}
+			return GameData.instance.EquipmentDataList.Find(x => x.id == keyboardEquipmentId);
+		}
+	}
+
+	public EquipmentData NextDrumEquipment
+	{
+		
+	}
+
+	public EquipmentData NextGuitarEquipment
+	{
+		
+	}
+
+	public EquipmentData NextBassEquipment
+	{
+		
+	}
+
+	public EquipmentData NextKeyboardEquipment
+	{
+		
+	}
 
 }
