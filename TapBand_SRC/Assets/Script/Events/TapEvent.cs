0e7dc29fd80523845a553f274c34aeaac83c5f8f
Zöldi-Kovács Róbert <zkovacs.robert@gmail.com>
2016-05-24 18:25:14 +0200

    stage prefabs, etc

---
+++ TapBand_SRC/Assets/Script/Events/TapEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public delegate void TapEvent(object sender, TapEventArgs e);
+
+public class TapEventArgs : EventArgs
+{
+    public TapEventArgs(float tapStrength)
+    {
+        TapStrength = tapStrength;
+    }
+
+    public float TapStrength { get; private set; }
+}
---
+++ TapBand_SRC/Assets/Script/Utils/UIInitializer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIInitializer : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // Turn off unnecessary UI elements
+        transform.FindChild("SettingsUI").gameObject.SetActive(false);
+
+        transform.FindChild("BackstageUI").gameObject.SetActive(false); ;
+
+        var dressingRoomUI = transform.FindChild("DressingRoomUI");
+        dressingRoomUI.FindChild("Drums").gameObject.SetActive(false);
+        dressingRoomUI.FindChild("Guitar1").gameObject.SetActive(false);
+        dressingRoomUI.FindChild("Guitar2").gameObject.SetActive(false);
+        dressingRoomUI.FindChild("Keyboards").gameObject.SetActive(false);
+        dressingRoomUI.gameObject.SetActive(false);
+    }
+}
